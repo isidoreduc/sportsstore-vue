1cdb0613c63016f7d4272857659faa4da84d920f
isidoreduc <isidoreducasse79@gmail.com>
2020-07-02 15:15:40 +0200

    product list

---
+++ src/components/ProductList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div v-for="item in products" :key="item.id" class="q-pa-sm ">
+      <q-card flat bordered>
+        <div class="text-h4 q-pa-sm">
+          {{ item.name }}
+
+          <q-chip
+            color="primary"
+            text-color="white"
+            icon="payments"
+            class="q-pa-md q-ma-sm float-right"
+          >
+            {{ item.price | currency }}
+          </q-chip>
+        </div>
+        <q-card-section>
+          {{ item.description }}
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['products']),
+  },
+  filters: {
+    currency(value) {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/Store.vue
+++ src/components/Store.vue
@@ -6,14 +6,25 @@
       <q-space></q-space>
       <q-btn flat round dense icon="whatshot" />
     </q-toolbar>
-    
+
     <div class="row">
       <div class="col-3 bg-dark q-pa-sm">
         <h4 class="text-white q-ma-sm">Categories</h4>
       </div>
-      <div class="col-9 bg-positive q-pa-sm">
-        <h4 class="text-white q-ma-sm">Products</h4>
+      <div class="col-9 q-pa-sm">
+        <product-list />
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import ProductList from './ProductList';
+export default {
+  components: {
+    ProductList,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
