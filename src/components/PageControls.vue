285163badf450d8c7ca33b5f76d33fe4669de706
isidoreduc <isidoreducasse79@gmail.com>
2020-07-02 22:42:05 +0200

    product list pagination

---
+++ src/components/PageControls.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-if="pageCount > 1" class="text-right">
+        <q-btn-group>
+            <q-btn
+                v-for="i in pageNumbers"
+                v-bind:key="i"
+                v-bind:color="colorButtonCurrentPage(i)"
+                v-on:click="setCurrentPage(i)"
+            >
+                {{ i }}
+            </q-btn>
+        </q-btn-group>
+    </div>
+</template>
+<script>
+    import { mapState, mapGetters, mapMutations } from 'vuex';
+    export default {
+        computed: {
+            ...mapState(['currentPage']),
+            ...mapGetters(['pageCount']),
+            pageNumbers() {
+                return [...Array(this.pageCount + 1).keys()].slice(1);
+            },
+        },
+        methods: {
+            ...mapMutations(['setCurrentPage']),
+            colorButtonCurrentPage(n) {
+                return n == this.currentPage ? 'primary' : 'dark';
+            },
+        },
+    };
+</script>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,43 +1,46 @@
 <template>
-  <div>
-    <div v-for="item in products" :key="item.id" class="q-pa-sm ">
-      <q-card flat bordered>
-        <div class="text-h4 q-pa-sm">
-          {{ item.name }}
+    <div>
+        <div v-for="item in products" :key="item.id" class="q-pa-sm ">
+            <q-card flat bordered>
+                <div class="text-h4 q-pa-sm">
+                    {{ item.name }}
 
-          <q-chip
-            color="primary"
-            text-color="white"
-            icon="payments"
-            class="q-pa-md q-ma-sm float-right"
-          >
-            {{ item.price | currency }}
-          </q-chip>
+                    <q-chip
+                        color="primary"
+                        text-color="white"
+                        icon="payments"
+                        class="q-pa-md q-ma-sm float-right"
+                    >
+                        {{ item.price | currency }}
+                    </q-chip>
+                </div>
+                <q-card-section>
+                    {{ item.description }}
+                </q-card-section>
+            </q-card>
         </div>
-        <q-card-section>
-          {{ item.description }}
-        </q-card-section>
-      </q-card>
+        <page-controls />
     </div>
-  </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+    import { mapGetters } from 'vuex';
+    import PageControls from './PageControls';
 
-export default {
-  computed: {
-    ...mapState(['products']),
-  },
-  filters: {
-    currency(value) {
-      return new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(value);
-    },
-  },
-};
+    export default {
+        components: { PageControls },
+        computed: {
+            ...mapGetters({products: "processedProducts"}),
+        },
+        filters: {
+            currency(value) {
+                return new Intl.NumberFormat('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                }).format(value);
+            },
+        },
+    };
 </script>
 
 <style lang="scss" scoped></style>
