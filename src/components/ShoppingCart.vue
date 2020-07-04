cfb4cdb63eadbf819b086c49164b8514fe4c3437
isidoreduc <isidoreducasse79@gmail.com>
2020-07-04 12:45:19 +0200

    shopping cart component

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="q-pa-md q-gutter-sm">
+        <q-toolbar class="bg-primary text-white">
+            <q-toolbar-title shrink class="absolute-center"
+                >SHOPPING CART</q-toolbar-title
+            >
+        </q-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {};
+</script>
+
+<style lang="scss" scoped></style>
